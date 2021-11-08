6fac0527a535042179ca56309b933f7841e82149
AllanDwyre <allan.dwyre@gmail.com>
2021-11-08 13:58:16 +0100

    main

--- ConsoleCpp/XorClass.cpp
+++ ConsoleCpp/XorClass.cpp
@@ -3,13 +3,13 @@
 #include <bitset>
 using namespace std;
 
-string XorClass::Crypt(string text, string decalage)
+string XorClass::Crypt(string text, string key)
 {
 	string result;
 	int current = 0;
 	for (char item : text)
 	{
-		result += (int)(bitset<8>(item) ^ bitset<8>(decalage[current % decalage.length()])).to_ulong();
+		result += (int)(bitset<8>(item) ^ bitset<8>(key[current % key.length()])).to_ulong();
 		current++;
 	}
 	return result;
